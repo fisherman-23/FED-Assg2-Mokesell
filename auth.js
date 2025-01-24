77fee9f0db4e8079f3ea1812c6110444fe9df94a
Jing Shun <ooijs234@gmail.com>
2025-01-25 00:12:15 +0800

    feat: fully implement login and signup

--- auth.js
+++ auth.js
@@ -3,16 +3,29 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase"; // Import the `auth` instance from firebase.js
 
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+const db = getFirestore();
 // Signup function
-export const signUp = async (email, password) => {
+export const signUp = async (email, password, username) => {
   try {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    const user = userCredential.user;
+
+    // Save the user's data (including username) to Firestore
+    await setDoc(doc(db, "users", user.uid), {
+      email: user.email,
+      createdAt: new Date(),
+      uid: user.uid,
+      username: username, // Adding the username to the Firestore document
+    });
+
     console.log("User signed up:", userCredential.user);
     return userCredential.user;
   } catch (error) {
@@ -47,3 +60,16 @@ export const logout = async () => {
     throw error;
   }
 };
+
+// Listen for authentication state changes
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in
+    console.log("User is signed in:", user);
+    console.log("User UID:", user.uid);
+    console.log("User Email:", user.email);
+  } else {
+    // No user is signed in
+    console.log("No user is signed in.");
+  }
+});
--- login.js
+++ login.js
@@ -1,7 +1,35 @@
 import { signUp, login, logout } from "./auth";
+// Login
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = loginForm["email"].value;
+  const password = loginForm["password"].value;
+  try {
+    await login(email, password);
+    loginForm.reset();
+    // go home screen
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 // Signup
-signUp("test@example.com", "password123")
-  .then((user) => console.log("Signup success:", user))
-  .catch((error) => console.error(error));
+const signupForm = document.getElementById("signup-form");
+signupForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const email = signupForm["email"].value;
+  const password = signupForm["password"].value;
+  const username = signupForm["username"].value;
+  try {
+    await signUp(email, password, username);
+    signupForm.reset();
+    // go home screen
+    window.location.href = "index.html";
+  } catch (error) {
+    console.error(error.message);
+  }
+});
 
 console.log("Signup function called");
