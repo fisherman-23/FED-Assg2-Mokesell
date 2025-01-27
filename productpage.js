ea8a6a4e7f8cd7bde56a508fd12e5a97c6f56d3e
Jing Shun <ooijs234@gmail.com>
2025-01-27 08:44:11 +0800

    perf: optimize progressive blur on images to improve scrolling perf

--- productpage.js
+++ productpage.js
@@ -1,72 +1,71 @@
-
-var productcontainer = document.querySelector('.product-card-container');
+var productcontainer = document.querySelector(".product-card-container");
 let numberOfCards = 0;
 let increment = 8;
 function loadProducts() {
-    fetch("products.json")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
+  fetch("products.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
     })
-    .then(data => {
-        if(numberOfCards+increment <= data.length) {
-            for (let i = numberOfCards; i < numberOfCards+increment; i++) {
-                const product = data[i];
-                const productdiv = document.createElement('div');
-                productdiv.className = 'product-card';
-                productdiv.innerHTML = `
+    .then((data) => {
+      if (numberOfCards + increment <= data.length) {
+        for (let i = numberOfCards; i < numberOfCards + increment; i++) {
+          const product = data[i];
+          const productdiv = document.createElement("div");
+          productdiv.className = "product-card";
+          productdiv.innerHTML = `
                     <div class="gradient-blur">
                         <section class="product-info-section">
                             <h2>${product.name}</h2>
                             <p>SGD ${product.price}</p>
                         </section>
-                        <img src="/productIMG/product${i+1}.png" alt="Product Image">
+                        <img src="/productIMG/product${
+                          i + 1
+                        }.png" alt="Product Image">
                         <div></div>
                         <div></div>
                         <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+
                     </div>
                 `;
-                productcontainer.appendChild(productdiv);
-            }
+          productcontainer.appendChild(productdiv);
         }
-        else{
-            for (let i = numberOfCards; i < data.length; i++) {
-                const product = data[i];
-                const productdiv = document.createElement('div');
-                productdiv.className = 'product-card';
-                productdiv.innerHTML = `
+      } else {
+        for (let i = numberOfCards; i < data.length; i++) {
+          const product = data[i];
+          const productdiv = document.createElement("div");
+          productdiv.className = "product-card";
+          productdiv.innerHTML = `
                     <div class="gradient-blur">
                         <section class="product-info-section">
                             <h2>${product.name}</h2>
                             <p>SGD ${product.price}</p>
                         </section>
-                        <img src="/productIMG/product${i+1}.png" alt="Product Image">
+                        <img src="/productIMG/product${
+                          i + 1
+                        }.png" alt="Product Image">
                         <div></div>
                         <div></div>
                         <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
+
                     </div>
                 `;
-                productcontainer.appendChild(productdiv);
-            }
-            document.querySelector('.load-more').style.display = 'none';
-            document.querySelector('.end').style.display = 'block';
+          productcontainer.appendChild(productdiv);
         }
-        numberOfCards = numberOfCards+increment; 
-    }).catch(error => {
-        console.error('Error loading products:', error);
+        document.querySelector(".load-more").style.display = "none";
+        document.querySelector(".end").style.display = "block";
+      }
+      numberOfCards = numberOfCards + increment;
+    })
+    .catch((error) => {
+      console.error("Error loading products:", error);
     });
 }
-document.addEventListener('DOMContentLoaded', function() {
-    loadProducts();
+document.addEventListener("DOMContentLoaded", function () {
+  loadProducts();
 });
-document.querySelector('.load-more').addEventListener('click', function() {
-    loadProducts();
+document.querySelector(".load-more").addEventListener("click", function () {
+  loadProducts();
 });
